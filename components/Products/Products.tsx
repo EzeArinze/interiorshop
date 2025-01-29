d4a590bd0b43847ecee8fccf1c2435a5d6c62a38
Arinze-Eze <nuelrinz@gmail.com>
2025-01-29 12:58:47 +0100

    refetchonwindowfocus and stale-time

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -54,6 +54,8 @@ function ProductsClient({ initialProducts = [] }) {
         pages: [initialProducts], // Set initial products here
         pageParams: [0],
       },
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5,
     });
 
   const products = data?.pages.flat() || [];
